185145cfad699ede7079b34e1b9e6c22a761091a
임성훈 <sasumpi123@naver.com>
2020-03-29 02:56:17 +0900

    API 작업중...

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -3,20 +3,55 @@ import "../styles/App.css";
 import axios from "axios";
 
 class App extends React.Component {
-  getData = async () => {
-    await axios.post(`/api/board`, {
-      title: "testTitle",
-      content: "testContent"
-    });
-    await axios.get(`/api/board`, {});
+  state = {
+    articles: []
   };
 
   componentDidMount() {
-    this.getData();
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const response = await axios.get("/api/board");
+    const articles = response.data.articles;
+    this.setState({ articles });
+  }
+
+  async delete(_id) {
+    await axios.delete("/api/board", {
+      data: {
+        _id
+      }
+    });
+    this.fetchData();
+  }
+
+  async update(_id) {
+    await axios.put("/api/board", {
+      _id,
+      title: "updated_title",
+      content: "updated_content"
+    });
+    this.fetchData();
+  }
+
+  articleList() {
+    const { articles } = this.state;
+
+    return articles.map((article, index) => {
+      return (
+        <div key={index}>
+          <div>{article.title}</div>
+          <div>{article.content}</div>
+          <button onClick={() => this.delete(article._id)}>Delete</button>
+          <button onClick={() => this.update(article._id)}>Update</button>
+        </div>
+      );
+    });
   }
 
   render() {
-    return <div className="App"></div>;
+    return <div className="App">{this.articleList()}</div>;
   }
 }
 
