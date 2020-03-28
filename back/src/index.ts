0cab611c98b149a759fcb841100222626714f166
bigdeal <bigdeal@smartstudy.co.kr>
2020-03-29 00:42:12 +0900

    mongoose 연결 완료.

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,11 @@
 import ApplicationServer from './classes/ApplicationServer';
+import { connectDB } from './utils/mongodb';
 
 async function main(): Promise<void> {
     const appServer: ApplicationServer = new ApplicationServer();
     appServer.initialize();
     appServer.open(8000);
+    connectDB();
 }
 
 main();
