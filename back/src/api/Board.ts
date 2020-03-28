544d214e2f8dde393544ed39192d577240f6a647
임성훈 <sasumpi123@naver.com>
2020-03-29 02:35:00 +0900

    API 작업 완료

--- back/src/api/Board.ts
+++ back/src/api/Board.ts
@@ -4,10 +4,49 @@ import Article from '../models/Article';
 
 const router: Router = Router();
 
+// 목록 조회
+// [GET] localhost:8000/api/board
 router.get('/',
     validate([]),
     async (request: Request, response: Response, next: NextFunction): Promise<void> => {
-        response.json({ data: '' });
+        const articles = await Article.find();
+        response.json({ articles });
+    }
+);
+
+// 생성
+// [POST] localhost:8000/api/board
+router.post('/',
+    validate([]),
+    async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+        const newArticle = new Article();
+        newArticle.title = request.body.title;
+        newArticle.content = request.body.content;
+        await newArticle.save();
+        response.json({});
+    }
+);
+
+// 수정
+// [PUT] localhost:8000/api/board
+router.put('/',
+    validate([]),
+    async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+        const article = await Article.findOne({ _id: request.body._id })
+        article.title = request.body.title;
+        article.content = request.body.content;
+        await article.save();
+        response.json({});
+    }
+);
+
+// 삭제
+// [DELETE] localhost:8000/api/board
+router.delete('/',
+    validate([]),
+    async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+        await Article.deleteOne({ _id: request.body._id });
+        response.json({});
     }
 );
 
